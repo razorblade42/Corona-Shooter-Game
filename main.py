e396a497d6e1cae0cfbd78f111e85e2fdcbdaf41
divyanx <divyansh.2488@gmail.com>
2020-11-28 04:27:06 +0530

    introduce falling objects

--- main.py
+++ main.py
@@ -46,13 +46,41 @@ class Player(pygame.sprite.Sprite):
         self.movement()
         self.boundary()
 
+class Corona(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,30))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0,WIDTH-self.rect.width)
+        self.rect.y = random.randrange(-150,-100)
+        self.speed_y = random.randrange(2,8)
+        self.speed_x = random.randrange(-3,3)  #x direction motion
+    def spawn_new_corona(self):
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-150, -100)
+        self.speed_y = random.randrange(2, 8)
+        self.speed_x = random.randrange(-3, 3)  # x direction motion
+    def boundary(self):
+         if self.rect.left >  WIDTH + 5 or self.rect.right < -5 or self.rect.top > HEIGHT + 5:
+            self.spawn_new_corona()
+    def update(self):
+        self.rect.y += self.speed_y
+        self.rect.x += self.speed_x
+        self.boundary()
 #Game Functions
 
 
 #Game sprites
 all_sprites = pygame.sprite.Group()
+all_corona = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+
+for i in range(9):
+    m = Corona()
+    all_corona.add(m)
+    all_sprites.add(m)
 #Main Game
 running = True
 while running:

85498fa4cf0c240faa8c5a9ed7184c05e0208bc3
divyanx <divyansh.2488@gmail.com>
2020-11-28 03:33:25 +0530

    Add intial files

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,0)
+
+#settings:
+pygame.init()
+pygame.mixer.init()
+WIDTH = 600
+HEIGHT = 800
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set.caption("Shoot-Karona!")
+clock = pygame.time.Clock()
+FPS = 60
+
+#Game Classes
+
+
+
+#Game Functions

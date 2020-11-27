4ac281ab237ca971d7d28cdae702a9a107ab7f4c
divyanx <divyansh.2488@gmail.com>
2020-11-28 03:54:46 +0530

    add player and sprites

--- main.py
+++ main.py
@@ -13,12 +13,46 @@ pygame.mixer.init()
 WIDTH = 600
 HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set.caption("Shoot-Karona!")
+pygame.display.set_caption("Shoot-Karona!")
 clock = pygame.time.Clock()
 FPS = 60
 
 #Game Classes
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,30))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT - 10  #sepration between bottom and ship
 
+    def update(self):
+        pass
 
 
 #Game Functions
+
+
+#Game sprites
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+#Main Game
+running = True
+while running:
+    clock.tick(FPS)
+
+    #check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #Update (for our sprites)
+    all_sprites.update()
+    #Draw/Render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    #Update the display
+    pygame.display.update()
+pygame.quit()
